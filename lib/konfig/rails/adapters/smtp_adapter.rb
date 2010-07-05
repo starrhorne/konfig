0a3fff189102b7aaa6d14a924837d3b27cf8eb31
Starr Horne <starr@chromahq.com>
2010-07-05 11:54:42 -0500

    remove leading underscore

--- lib/konfig/rails/adapters/smtp_adapter.rb
+++ lib/konfig/rails/adapters/smtp_adapter.rb
@@ -2,7 +2,7 @@ module Konfig
   class SmtpAdapter < Adapter
 
     def adapt
-      using(:_smtp) do |data|
+      using(:smtp) do |data|
         ActionMailer::Base.smtp_settings = data[Rails.env]
       end
     end
